276f0ff7f4cadb97e727f75d17ba2ce4da5ed551
proguy914629 <github@proguy.me>
2023-01-01 18:29:26 +0700

    fix stuff

--- src/script.js
+++ src/script.js
@@ -4,59 +4,81 @@ const r_title = document.getElementById("res-title");
 const r_artist = document.getElementById("res-artist");
 const r_lyrics = document.getElementById("res-lyrics");
 const loading = document.querySelector(".lds-ring");
-const result = document.querySelector(".result");
+
+let disableAutocomplete = false;
+
+function getInput(element) {
+    element = element || input;
+    let value = element.value;
+    value = value.trim();
+    return value;
+}
 
 async function getLyrics(query) {
+    query = query.trim();
     let r = await fetch(`https://api.yodabot.xyz/api/lyrics/search?q=${query}`);
     return await r.json();
 }
 
 async function performAutocomplete(query) {
+    query = query.trim();
     let r = await fetch(`https://api.yodabot.xyz/api/lyrics/suggest?q=${query}&amount=5`);
     return await r.json();
 }
 
 async function displayLyrics(query) {
-    if (!query) return;
+    query = query.trim();
 
-    result.style.display = "none";
+    if (!query) {
+        alert("Please enter a query!");
+        return
+    }
+
+    input.readOnly = true;
+    disableAutocomplete = true;
 
     r_title.innerText = "";
     r_artist.innerText = "";
     r_lyrics.innerText = "";
 
-    let oriAutocomplete = autocomplete.innerHTML;
     autocomplete.innerHTML = "";
 
     loading.style.display = "inline-block";
     let lyrics = await getLyrics(query);
     loading.style.display = "none";
 
-    if (!lyrics.title || !lyrics.artist || !lyrics.lyrics) {
-        autocomplete.innerHTML = oriAutocomplete;
-        return alert("Lyrics not found.");
+    input.readOnly = false;
+    disableAutocomplete = false;
+
+    if (!lyrics || !lyrics.title || !lyrics.artist || !lyrics.lyrics) {
+        alert("No lyrics found. Sorry!");
+        return;
     }
 
     input.value = "";
+    autocomplete.innerHTML = "";
 
     r_title.innerText = lyrics.title;
     r_artist.innerText = lyrics.artist;
     r_lyrics.innerText = lyrics.lyrics;
-
-    result.style.display = "block";
 }
 
 async function displayAutocomplete(e) {
-    if (e.target.value === "") {
+    let value = getInput(e.target);
+
+    if (value === "") {
         autocomplete.innerHTML = "";
         return;
     }
 
-    let suggestions = await performAutocomplete(e.target.value);
+    if (disableAutocomplete) return;
+
+    let suggestions = await performAutocomplete(value);
     autocomplete.innerHTML = "";
     for (let suggestion of suggestions) {
         let li = document.createElement("li");
         li.addEventListener('click', async function (e) {
+            window.disableAutocomplete = true;
             input.value = e.target.innerText;
             await displayLyrics(e.target.innerText);
         });
@@ -69,8 +91,9 @@ input.addEventListener('input', displayAutocomplete);
 input.addEventListener('propertychange', displayAutocomplete);
 input.addEventListener('change', displayAutocomplete);
 input.addEventListener('keydown', async function (e) {
-    if (e.code === 'Enter' && e.target.value !== "") {
-        await displayLyrics(input.value);
+    if (e.code === 'Enter') {
+        window.disableAutocomplete = true;
+        await displayLyrics(getInput(e.target));
     }
 });
 setInterval(() => {
